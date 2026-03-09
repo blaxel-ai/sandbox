de61b422df5eb2c868c7a2107ba85dc455f611f0
cploujoux <ch.ploujoux@gmail.com>
2026-03-09 15:09:30 -0700

    Revert "Move to json logger"
    This reverts commit b4c19f6af75394b637a97d5b99d585875aca2e19.

--- sandbox-api/main.go
+++ sandbox-api/main.go
@@ -32,7 +32,9 @@ import (
 // @name Authorization
 // @BasePath        /
 func main() {
-	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableColors: true,
+	})
 	logrus.SetLevel(logrus.DebugLevel)
 
 	// Load .env file
